bed17373339548f3272ca5a2deb827da76d86d63
kawasa9350-svg <kawasa9350@gmail.com>
2026-03-05 02:20:56 -0500

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-node --openssl-legacy-provider index.js
+node --tls-min-v1.2 --openssl-legacy-provider index.js
 echo Phoenix Assistance Bot has crashed. Restarting in 5 seconds...
 timeout /t 5
 goto loop
